0639b8c8fa6debc0c4f934fef4b34dd21efa77a4
Marcin <wanago.marcin@gmail.com>
2024-01-27 23:09:38 +0100

    feat(Articles): add the migration with the articles_from_yesterday

---
+++ src/migrations/20240127213806_add_articles_from_yesterday_view.ts
@@ -0,0 +1,18 @@
+import { Migration, sql } from 'kysely';
+
+export const up: Migration['up'] = async (database) => {
+  await database.schema
+    .createView('articles_from_yesterday')
+    .as(
+      database
+        .selectFrom('articles')
+        .selectAll()
+        .where('created_at', '<', sql`NOW() - INTERVAL '1 DAY'`)
+        .where('created_at', '>', sql`NOW() - INTERVAL '2 DAYS'`),
+    )
+    .execute();
+};
+
+export const down: Migration['down'] = async (database) => {
+  await database.schema.dropView('articles_from_yesterday');
+};
